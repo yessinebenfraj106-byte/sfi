80d216577577ee03d370b0b2a0ee817e7081674e
Yessine Ben Fraj <your.github.email@example.com>
2026-09-03 20:34:18 +0100

    feat: setup database tables and add phone number to registration

---
+++ src/lib/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+// These variables must match exactly what is inside your .env.local file
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
